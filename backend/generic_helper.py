7e24eb5e185b572f4774e8ea272af19c95ccf856
Prasad Kanakgiri <kanakgiriprasad26@gmail.com>
2024-12-03 14:44:34 +0530

    backend first commit

---
+++ backend/generic_helper.py
@@ -0,0 +1,20 @@
+# Author: Dhaval Patel. Codebasics YouTube Channel
+
+import re
+
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+
+def extract_session_id(session_str: str):
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string = match.group(1)
+        return extracted_string
+
+    return ""
+
+if __name__ == "__main__":
+    print(get_str_from_food_dict({"samosa": 2, "chole": 1}))
+    # print(extract_session_id("projects/mira-chatbot-for-food-del-qvq9/agent/sessions/beb50dbd-ca97-173e-ba2f-55340b7263dc/contexts/ongoing-order"))
